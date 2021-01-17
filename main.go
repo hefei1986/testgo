fa0f2a3fb0b03447df6fb03490ea50945bf8fd75
hefei1986 <hefeimissyou@163.com>
2021-01-17 12:41:21 +0800

    Update main.go
    bug fixed. need create bucket before using.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/boltdb/bolt"
 )
@@ -11,36 +12,39 @@ func main() {
 	// Open the my.db data file in your current directory.
 	// It will be created if it doesn't exist.
 	// 创建或者打开数据库
-	db, err := bolt.Open("my.db", 0600, nil)
+	db, err := bolt.Open("data\\my.db", 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
-
-	// 创建表
+	log.Println(db.Path())
+	log.Println(db.GoString())
 	err = db.Update(func(tx *bolt.Tx) error {
-		// 获取BlockBucket表单
-		b := tx.Bucket([]byte("BlockBucket"))
 
-		// 往表里面存储数据
-		if b != nil {
-			err := b.Put([]byte("ll"), []byte("Send 1000 BTC To 冠希哥......"))
-			if err != nil {
-				log.Panic("数据存储失败......")
-			}
-		}
-
-		// 返回nil，以便数据库处理相应操作
 		return nil
 	})
-	//更新失败
+	defer db.Close()
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b, e := tx.CreateBucketIfNotExists([]byte("BlockBucket"))
+		if e != nil {
+			log.Fatalln("Failed create new bucket", e)
+		}
+
+		err := b.Put([]byte("ll"), []byte("Send 1000 BTC To 冠希哥......"))
+		if err != nil {
+			log.Panic("数据存储失败......")
+		}
+
+		return nil
+	})
+
 	if err != nil {
 		log.Panic(err)
 	}
 
-	// 查看数据
-	err = db.View(func(tx *bolt.Tx) error {
+	time.Sleep(1 * time.Second)
 
+	err = db.View(func(tx *bolt.Tx) error {
 		// 获取BlockBucket表对象
 		b := tx.Bucket([]byte("BlockBucket"))
 
